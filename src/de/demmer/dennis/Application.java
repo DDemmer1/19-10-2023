b9a49f68d1bedd9a1ac6e37dd717dcabcdb4865c
Dennis Demmer <Dennis.Demmer@web.de>
2023-10-19 18:27:55 +0200

    Update Application.java

--- src/de/demmer/dennis/Application.java
+++ src/de/demmer/dennis/Application.java
@@ -2,12 +2,43 @@ package de.demmer.dennis;
 
 public class Application {
 
+	// main -> STRG + LEERTASTE
+
+	/*
+	 * Das ist ein Kommentar
+	 */
+	
+	// STRG + SHIFT +F -> auto format
+
 	
 	public static void main(String[] args) {
+//		System.out.println("Hallo Welt");
+		
+//		boolean b = 2 + 5 == 7;
+		boolean b;
+		
+		b = true;
+		System.out.println(b);
+		
+		b = false;
+		System.out.println(b);
+		
+		
+		boolean a = false;
+		int i = 5;
+		
+		//STRG + 7 -> Zeile zu kommentar
+//		if(2 + 2 == 5) {
+//      if(b == true) {
+//		if(a == b) {
+		
+		if(i >= 8 ) {
+			//sysout + STRG + LEERTASTE
+//			System.out.println("if wurde getriggert");
+		}
+		
 		
 		
 		
 	}
-	
-	
 }
